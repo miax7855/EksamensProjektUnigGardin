6159e90d7540f7bece126de536237cbdb60901f1
Mia Pars Markussen <miax7855@edu.eal.dk>
2019-04-25 09:33:26 +0200

    order, order repo klasser

---
+++ EksamensProjektUnigGardin/ImportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjektUnigGardin
+{
+    public class ImportController
+    {
+        public void readLines()
+        {
+            string[] text = File.ReadAllLines("Orders.txt");
+
+            
+        }
+
+    }
+}
