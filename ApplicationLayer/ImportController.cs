cc9f9d28100ef590645ac5d9db49f261fc0612db
Asbjørn Larsen <asbjorn_andreas_larsen@hotmail.com>
2019-04-25 12:34:10 +0200

    implementeret dele af importController

--- ApplicationLayer/ImportController.cs
+++ ApplicationLayer/ImportController.cs
@@ -9,12 +9,42 @@ namespace EksamensProjektUnigGardin
 {
     public class ImportController
     {
-        public void readLines()
+		string[] text;
+		string[] orderItems;
+		string[] sampleTypeArray;
+		List<string> sampleTypeList;
+
+		public void readLines()
         {
             string[] text = File.ReadAllLines("Orders.txt");
 
             
         }
+		public void RegisterOrders()
+		{
+			foreach (string item in text)
+			{
+				string[] orderItems = item.Split(';');
+				string[] sampleTypeArray = orderItems[7].Split(',');
+				ConvertArrayToList(sampleTypeArray);
+				Order order = new Order(orderItems[0], orderItems[1], Convert.ToInt32(orderItems[2]), orderItems[3], orderItems[4], Convert.ToInt32(orderItems[5]), orderItems[6], sampleTypeList);
+			
 
-    }
+
+			}
+		}
+		public List<string> ConvertArrayToList(string[] c)
+		{
+			List<string> ListConvert = new List<string>();
+			foreach (string item in c)
+			{
+				ListConvert.Add(item);
+			}
+
+			return ListConvert;
+		}
+
+
+
+	}
 }
