3bce397ff6f5c9efc7e75cc8abdb339ca6aee949
Asbjørn Larsen <asbjorn_andreas_larsen@hotmail.com>
2019-05-06 09:18:47 +0200

    test stuff

--- ImportControllerUnitTest/ErrorLogsTest.cs
+++ ImportControllerUnitTest/ErrorLogsTest.cs
@@ -12,14 +12,15 @@ namespace ImportControllerUnitTest
 		[TestMethod]
 		public void TestSaveErrorLog()
 		{
+			bool True = true;
 			string actualString;
 			ErrorController errorsTest = new ErrorController();
-
-			string expectedString = "THERE ARE OVER 9000 ERRORS!";
-			errorsTest.SaveErrorLog(expectedString);
+			string expectedString = "THERE ARE OVER 9000 ERRORS!\n///\n///\n///";
+			errorsTest.TestSaveErrorLog(expectedString);
 			actualString = File.ReadAllText("ErrorLog.txt");
 
 			Assert.AreEqual(expectedString, actualString);
+			//Assert.AreEqual(True, expectedString.Equals(actualString));
 		}
 	}
 }
