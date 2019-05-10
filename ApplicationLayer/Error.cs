a83e13fcab5507c3696862819e2e3580d23a72f7
Julian <juli11c0@edu.eal.dk>
2019-05-10 12:04:28 +0200

    bearbejdet constructor i Order

---
+++ ApplicationLayer/Error.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer
+{
+	public class ErrorController
+	{
+		public void SaveErrorLog(string message)
+		{
+			ImportController ic = new ImportController();
+			string fileName = "ErrorLog.txt";
+			string relativePath = ic.GetFilePath(fileName);
+			Controller c = new Controller();
+			using (StreamWriter sw = new StreamWriter(relativePath))
+				sw.WriteLine(DateTime.Now + "\n" + message + "\n///\n///\n///");
+		}
+
+		// ingen datetime.now så den kan testes
+		public void TestSaveErrorLog(string message)
+		{
+			ImportController ic = new ImportController();
+			string fileName = "ErrorLog.txt";
+			string relativePath = ic.GetFilePath(fileName);
+			Controller c = new Controller();
+			using (StreamWriter sw = new StreamWriter(relativePath))
+				sw.WriteLine(message + "\n///\n///\n///");
+		}
+        
+	}
+}
